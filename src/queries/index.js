bf5b05d0b69684a0fa96ef89a932535351ef4064
sobismhs <sobia.zulfiqar@codistan.org>
2023-12-05 14:30:55 +0500

    new apis

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,8 +1,12 @@
 import sellerOrderCount from "./sellerOrderCount.js";
 import sellerUploadCount from "./sellerUploadCount.js"
+import uploadedProductsMonthly from "./uploadedProductsMonthly.js";
+import sellerRegistraionMonthly from "./sellerRegistraionMonthly.js";
 
 export default {
     sellerOrderCount,
-    sellerUploadCount
+    sellerUploadCount,
+    uploadedProductsMonthly,
+    sellerRegistraionMonthly
 
 }
