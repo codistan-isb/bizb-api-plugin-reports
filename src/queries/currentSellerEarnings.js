d277edf53852c2f4a4a016626aade08e67b745ae
RehanMaroof <rehan.maroof@codistan.org>
2024-01-30 15:49:06 +0500

    update: sellerEarning in graph form

--- src/queries/currentSellerEarnings.js
+++ src/queries/currentSellerEarnings.js
@@ -27,7 +27,7 @@ export default async function currentSellerEarnings(
 
     let query = {
       createdAt: {
-        $gte:  formattedStartDate,
+        $gte: formattedStartDate,
         $lte: formattedEndDate,
       },
       sellerId: _id,
@@ -39,11 +39,12 @@ export default async function currentSellerEarnings(
     console.log("currentSellerEarnings", currentSellerEarnings);
 
     // Group payments by week
+    // Group payments by week
     const earningsByWeek = currentSellerEarnings.reduce((acc, doc) => {
       const weekStartDate = moment(doc.createdAt)
         .startOf("week")
         .utc()
-        .format("ddd, DD MMM YYYY HH:mm:ss [GMT]");
+        .format("YYYY-MM-DDTHH:mm:ss[Z]");
 
       if (!acc[weekStartDate]) {
         acc[weekStartDate] = 0;
@@ -56,30 +57,29 @@ export default async function currentSellerEarnings(
     // Calculate the week numbers dynamically based on the start and end dates
     const firstWeekStart = moment(formattedStartDate).startOf("week");
     const lastWeekEnd = moment(formattedEndDate).endOf("week");
-    const weeks = [];
+    const weekData = [];
 
     let currentWeekStart = firstWeekStart;
+    let weekNumber = 1;
     while (currentWeekStart.isBefore(lastWeekEnd)) {
-      const weekStartDate = currentWeekStart.utc().format("ddd, DD MMM YYYY HH:mm:ss [GMT]");
-      const weekEndDate = moment(currentWeekStart).endOf("week").utc().format("ddd, DD MMM YYYY HH:mm:ss [GMT]");
-      const totalEarnings = earningsByWeek[weekStartDate] || "0";
+      const weekStartDate = currentWeekStart.utc().format("YYYY-MM-DDTHH:mm:ss[Z]");
+      const weekEndDate = moment(currentWeekStart).endOf("week").utc().format("YYYY-MM-DDTHH:mm:ss[Z]");
+      const totalEarnings = earningsByWeek[weekStartDate] || 0;
 
-      weeks.push({
-        totalEarnings,
+      weekData.push({
+        name: `Week ${weekNumber}, ${currentWeekStart.format("D MMM")}-${moment(currentWeekStart).endOf("week").format("D MMM")}`,
+        uv: totalEarnings,
+        pv: totalEarnings,
+        amt: totalEarnings,
       });
 
       currentWeekStart.add(1, "week");
+      weekNumber += 1;
     }
 
-    // Transform the array of week objects into the final result object
-    const weeklyEarnings = weeks.reduce((acc, week, index) => {
-      acc[`week${index + 1}Earnings`] = week.totalEarnings || "0";
-      return acc;
-    }, {});
+    console.log("Week Data", weekData);
 
-    console.log("Weekly Earnings", weeklyEarnings);
-
-    return weeklyEarnings;
+    return { weekData };
   } catch (error) {
     console.log(error);
     throw new Error("Error fetching current seller earnings");
