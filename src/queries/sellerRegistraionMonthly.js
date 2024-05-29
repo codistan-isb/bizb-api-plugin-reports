fe7f34cbd20e0c9a0c8cd8f9238da109d585c9d6
sobismhs <sobia.zulfiqar@codistan.org>
2024-05-29 14:04:55 +0500

    promoCode filter on seller registration table

--- src/queries/sellerRegistraionMonthly.js
+++ src/queries/sellerRegistraionMonthly.js
@@ -1,13 +1,12 @@
+import pkg from 'mongodb'; // or ObjectID 
+const { ObjectId } = pkg;
 
-export default async function sellerRegistraionMonthly(parent, args, context, info) {
-    let { startDate, endDate, skip, limit, storeName, email, contact } = args;
-    let { collections } = context
-    let { Accounts } = collections;
+export default async function sellerRegistrationMonthly(parent, args, context, info) {
+    let { startDate, endDate, skip, limit, storeName, email, contact, promoCode } = args;
+    let { collections } = context;
+    let { Accounts, SellerDiscounts } = collections;
     let match = {};
 
-    // let uploadedProductsMonthly = await Products.find({}).toArray();
-
-    // console.log("uploadedProductsMonthly",startDate, endDate, skip, limit)
     if (storeName) {
         match['storeName'] = storeName;
     }
@@ -18,23 +17,45 @@ export default async function sellerRegistraionMonthly(parent, args, context, in
         match['contactNumber'] = contact;
     }
 
-    let query = {
-        "createdAt": {"$gte": startDate, "$lte": endDate}
+    let query  = {
+        "createdAt": { "$gte": new Date(startDate), "$lte": new Date(endDate) }
+    };
+    query = { ...query , ...match };
+    console.log("Initial query: ", query);
+
+    if (promoCode) {
+        console.log("Promo code: ", promoCode);
+        const promoCodeFilter = { code: promoCode };
+        console.log("PromoCodeFilter: ", promoCodeFilter);
+
+        const sellersWithPromo = await SellerDiscounts.find(promoCodeFilter).toArray();
+        console.log("Sellers with promo code: ", sellersWithPromo);
+
+        const sellerIds = sellersWithPromo.map(seller => seller.sellerId);
+        console.log("Seller IDs: ", sellerIds);
+        console.log("Seller IDs type: ", typeof(sellerIds));
+
+
+        if (sellerIds.length > 0) {
+            query['_id'] = sellerIds.length === 1 ? sellerIds[0] : { $in: sellerIds };
+        } else {
+            // If no sellers found with the given promo code, return empty results early
+            return {
+                totalcount: 0,
+                seller: []
+            };
+        }
     }
-    query = { ...query, ...match };
+    console.log("Final query: ", query);
 
-    let total_count = await Accounts.count(query)
+    let total_count = await Accounts.countDocuments(query);
+    console.log("Total count: ", total_count);
 
-    let sellerRegistraionMonthly = await Accounts.find(query).skip(skip).limit(limit).toArray();
-
-    // cursor = collection.find(query).skip(skip).limit(limit)
-
-    // result = list(cursor)
+    let sellerRegistrationMonthly = await Accounts.find(query).skip(skip).limit(limit).toArray();
+    console.log("Seller registration monthly: ", sellerRegistrationMonthly);
 
     return {
-        totalcount: total_count ,
-        seller : sellerRegistraionMonthly
+        totalcount: total_count,
+        seller: sellerRegistrationMonthly
     };
-   
-    
 }
