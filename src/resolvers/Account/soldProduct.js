8c46bee561db567baf6120fd548c96dc99b788f0
Ramzan316 <Muhammadramzan@codistan.org>
2025-03-06 09:59:29 +0500

    Fix:Fetchingcity

--- src/resolvers/Account/soldProduct.js
+++ src/resolvers/Account/soldProduct.js
@@ -1,18 +1,18 @@
 export default async function soldProduct(parent, args, context, info) {
-    console.log("parent in soldProduct", parent);
-    let {  collections } = context;
-  let { SimpleInventory,  Products ,Accounts} = collections;
+  console.log("parent in soldProduct", parent);
+  let { collections } = context;
+  let { SimpleInventory, Products, Accounts } = collections;
   try {
-    if (parent ) {
-        var id = parent?._id;
-        console.log("userId", id);
-        // Rest of your code
-      } else {
-        throw new Error("Parent object or _id property is undefined");
-      }
+    if (parent) {
+      var id = parent?._id;
+      console.log("userId", id);
+      // Rest of your code
+    } else {
+      throw new Error("Parent object or _id property is undefined");
+    }
     console.log("userId", id);
-    const account = await Accounts.findOne({_id:id})
-      console.log("account",account)
+    const account = await Accounts.findOne({ _id: id })
+    // console.log("account billing", account.billing)
     // Step 1: Fetch all products for the given seller ID
     const allProducts = await Products.find({ sellerId: account?.userId }).toArray();
     // console.log("allProducts", allProducts);
@@ -25,19 +25,19 @@ export default async function soldProduct(parent, args, context, info) {
       inventoryInStock: 0,
       "productConfiguration.productId": { $in: productIds },
     }).toArray();
-  
+
 
     // Step 3: Count the number of sold products
     const soldProductCount = soldProducts.length;
     console.log("soldProductCount", soldProductCount);
-    const soldProduct ={
-        totalSoldProductCount : soldProductCount
+    const soldProduct = {
+      totalSoldProductCount: soldProductCount
     }
     return soldProduct;
-}
-catch (error) {
+  }
+  catch (error) {
     console.log("error", error);
     throw new Error(error);
-}
+  }
 
 }
